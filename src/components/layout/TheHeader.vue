f712910a2400e7419044680a84643b2f1c25e514
Steve Hobbs <steve@Steves-MacBook.local>
2020-10-21 21:28:32 +0100

    Initial commit

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ['title']
+}
+</script>
+
+<style scoped>
+  header {
+    width: 100%;
+    height: 5rem;
+    margin-bottom: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #39116d;
+  }
+
+  header h1 {
+    color: white;
+    margin: 0;
+  }
+</style>
